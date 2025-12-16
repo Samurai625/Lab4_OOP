048f82d06ccdae49c19889f01407c4dd60725bad
Samurai625 <vovik12on@gmail.com>
2025-12-16 21:18:24 +0200

    Add private void Form1_Load in Form1.cs

--- OOP_Lab_4/Form1.cs
+++ OOP_Lab_4/Form1.cs
@@ -6,5 +6,16 @@ namespace OOP_Lab_4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Circle circle = new Circle(50, 100, 20, this);
+            Square square = new Square(50, 200, 40, this);
+            Rhomb rhomb = new Rhomb(50, 300, 60, 40, this);
+
+            circle.MoveRight(50);
+            square.MoveRight(50);
+            rhomb.MoveRight(50);
+        }
     }
 }
