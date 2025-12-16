7f36911091da0d68660ad844a161a6d2444436c1
Samurai625 <vovik12on@gmail.com>
2025-12-16 21:26:03 +0200

    Refact Form1.cs, del Form1_Load, add logic with click button

--- OOP_Lab_4/Form1.cs
+++ OOP_Lab_4/Form1.cs
@@ -2,17 +2,22 @@ namespace OOP_Lab_4
 {
     public partial class Form1 : Form
     {
+
+        private Circle circle;
+        private Square square;
+        private Rhomb rhomb;
+
         public Form1()
         {
             InitializeComponent();
+
+            circle = new Circle(50, 100, 20, this);
+            square = new Square(50, 200, 40, this);
+            rhomb = new Rhomb(50, 300, 40, 40, this);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnStart_Click(object sender, EventArgs e)
         {
-            Circle circle = new Circle(50, 100, 20, this);
-            Square square = new Square(50, 200, 40, this);
-            Rhomb rhomb = new Rhomb(50, 300, 60, 40, this);
-
             circle.MoveRight(50);
             square.MoveRight(50);
             rhomb.MoveRight(50);
