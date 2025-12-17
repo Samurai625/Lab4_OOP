22e98929e207d31e12ff0cfbb113e80851ea2cf2
Samurai625 <vovik12on@gmail.com>
2025-12-17 13:46:52 +0200

    Refact Form1.cs

--- OOP_Lab_4/Form1.cs
+++ OOP_Lab_4/Form1.cs
@@ -3,24 +3,23 @@ namespace OOP_Lab_4
     public partial class Form1 : Form
     {
 
-        private Circle circle;
-        private Square square;
-        private Rhomb rhomb;
+        List<Figure> figures = new List<Figure>();
 
         public Form1()
         {
             InitializeComponent();
 
-            circle = new Circle(50, 100, 20, this);
-            square = new Square(50, 200, 40, this);
-            rhomb = new Rhomb(50, 300, 40, 40, this);
+            figures.Add(new Circle(50, 100, 20, this));
+            figures.Add(new Square(50, 200, 40, this));
+            figures.Add(new Rhomb(50, 300, 40, 40, this));
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            circle.MoveRight(50);
-            square.MoveRight(50);
-            rhomb.MoveRight(50);
+            foreach(var Shape in figures)
+            {
+                Shape.MoveRight(50);
+            }
         }
     }
 }
